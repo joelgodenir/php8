fbacd5e2dde81ae14fe1dc6b6052cfc5ecce90a3
Pierre Barré <contact@pierreetnico.onmicrosoft.com>
2021-10-28 12:35:15 +0200

    commit

--- fonctions.php
+++ fonctions.php
@@ -11,7 +11,10 @@ function monFonction() {
 monFonction();
 
 
-// Déclaration d'une fonction avec argument. 
+/* Déclaration d'une fonction avec argument. 
+Noter un ou plusieurs paramètres dans les parenthèses de la fonction. 
+Lors de l'appel de la fonction ajouter les arguments utiliser lors de l'execution de la fonction.
+*/
 
 function maFonction($a) {
     echo $a;
