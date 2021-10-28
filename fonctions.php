8111206ec5e92acecdc0cd6ec2feb03d3bb36a48
Pierre Barré <contact@pierreetnico.onmicrosoft.com>
2021-10-28 13:43:12 +0200

    commit

--- fonctions.php
+++ fonctions.php
@@ -22,3 +22,25 @@ function maFonction($a) {
 
 maFonction("coucou");
 
+// Afficher plusieurs paramètres
+
+function ma_Fonction() {
+    $recuperatArguments = func_get_args(); /* Récupère tous les arguments qui vont être déclaré au moment de l'appel de la fonction et les ranger en Array */
+
+    $choisiLargument = func_get_arg(1); /* Avec arg je décide de l'élément que je veux récupérer. En l'occurence je vais récupérer l'argument 2 dans le tableau soit 1 */
+    echo $choisiLargument;
+
+    return $recuperatArguments; /* return permet de retourner une valeur, sans forcément l'afficher avec par exemple un echo */
+}
+
+ma_Fonction(10,1,"coucou"); 
+
+
+
+// Nouvelle génération avec rest
+
+function mon_Fonction(...$arg){ // les ... résumes plus rapidement le type juste au dessu, elle récupère tous les paramètres et les ajoutes à un tableau
+    print_r($arg);
+}
+
+mon_Fonction(1,5,9);
